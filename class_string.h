c1c58d42481212479bccdf54ae9105e5554a4699
Robert Strandh <robert.strandh@gmail.com>
2016-05-28 10:32:11 +0200

    New component containing external definition of the class STRING.

---
+++ class_string.h
@@ -0,0 +1,9 @@
+#ifndef CLASS_STRING_H
+#define CLASS_STRING_H
+
+extern struct header_struct class_string_header;
+
+#define reference_string(name)                       \
+  (void *) &string_ ## name ## _header
+
+#endif
