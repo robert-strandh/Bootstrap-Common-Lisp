458459a768fc6a6171c21569ce66cc9430d6b0a8
Robert Strandh <robert.strandh@gmail.com>
2016-05-28 10:35:11 +0200

    New component containing definitions of some commonly-used integers.

---
+++ integers.c
@@ -0,0 +1,120 @@
+#include "class_integer.h"
+#include "class_symbol.h"
+#include "class_class.h"
+#include "integers.h"
+#include "symbols.h"
+
+void *
+integer_1_rack[] =
+  {
+    reference_symbol(nil), 
+    reference_integer(1),
+    (void *) 1,
+  };
+
+struct header_struct integer_1_header =
+  {
+    reference_class(integer),
+    integer_1_rack,
+  };
+
+define_integer(0);
+define_integer(2);
+define_integer(3);
+define_integer(4);
+define_integer(5);
+define_integer(6);
+define_integer(7);
+define_integer(8);
+define_integer(9);
+define_integer(10);
+define_integer(11);
+define_integer(12);
+define_integer(13);
+define_integer(14);
+define_integer(15);
+define_integer(16);
+define_integer(17);
+define_integer(18);
+define_integer(19);
+define_integer(20);
+define_integer(21);
+define_integer(22);
+define_integer(23);
+define_integer(24);
+define_integer(25);
+define_integer(26);
+define_integer(27);
+define_integer(28);
+define_integer(29);
+define_integer(30);
+define_integer(31);
+define_integer(32);
+define_integer(33);
+define_integer(34);
+define_integer(35);
+define_integer(36);
+define_integer(37);
+define_integer(38);
+define_integer(39);
+define_integer(40);
+define_integer(41);
+define_integer(42);
+define_integer(43);
+define_integer(44);
+define_integer(45);
+define_integer(46);
+define_integer(47);
+define_integer(48);
+define_integer(49);
+define_integer(50);
+define_integer(51);
+define_integer(52);
+define_integer(53);
+define_integer(54);
+define_integer(55);
+define_integer(56);
+define_integer(57);
+define_integer(58);
+define_integer(59);
+define_integer(60);
+define_integer(61);
+define_integer(62);
+define_integer(63);
+define_integer(64);
+define_integer(65);
+define_integer(66);
+define_integer(67);
+define_integer(68);
+define_integer(69);
+define_integer(70);
+define_integer(71);
+define_integer(72);
+define_integer(73);
+define_integer(74);
+define_integer(75);
+define_integer(76);
+define_integer(77);
+define_integer(78);
+define_integer(79);
+define_integer(80);
+define_integer(81);
+define_integer(82);
+define_integer(83);
+define_integer(84);
+define_integer(85);
+define_integer(86);
+define_integer(87);
+define_integer(88);
+define_integer(89);
+define_integer(90);
+define_integer(91);
+define_integer(92);
+define_integer(93);
+define_integer(94);
+define_integer(95);
+define_integer(96);
+define_integer(97);
+define_integer(98);
+define_integer(99);
+define_integer(100);
