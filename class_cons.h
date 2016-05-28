2bb504d8ffc38b3d35966eab0fe264358ae98c0b
Robert Strandh <robert.strandh@gmail.com>
2016-05-28 10:31:08 +0200

    New component containg accessors for the class CONS.

---
+++ class_cons.h
@@ -0,0 +1,27 @@
+#ifndef CLASS_CONS_H
+#define CLASS_CONS_H
+
+#include "header.h"
+
+extern struct header_struct class_cons_header;
+
+#define define_cons(name, car, cdr)		     \
+                                                     \
+void *                                               \
+cons_ ## name ## _rack[] =                           \
+  {                                                  \
+    (void *) &symbol_nil_header,                     \
+    car,                                             \
+    cdr,                                             \
+  };                                                 \
+                                                     \
+struct header_struct cons_ ## name ## _header =      \
+  {                                                  \
+    &class_cons_header,                              \
+    cons_ ## name ## _rack,		             \
+  }
+
+#define reference_cons(name)                         \
+  (void *) &cons_ ## name ## _header
+
+#endif
