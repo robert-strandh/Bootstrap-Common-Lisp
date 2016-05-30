4b6e8a1b51d2c549e332b2a4275bd8c41a214997
Robert Strandh <robert.strandh@gmail.com>
2016-05-30 11:08:04 +0200

    Add macro for declaring an integer .

--- class_integer.h
+++ class_integer.h
@@ -2,21 +2,24 @@
 
 extern struct header_struct class_integer_header;
 
-#define define_integer(value)                        \
-                                                     \
-void *                                               \
-integer_ ## value ## _rack[] =                       \
-  {                                                  \
-    (void *) &symbol_nil_header,                     \
-    (void *) &integer_1_header,                      \
-    (void *) value,                                  \
-  };                                                 \
-                                                     \
-struct header_struct integer_ ## value ## _header =  \
-  {                                                  \
-    &class_integer_header,                           \
-    integer_ ## value ## _rack,		             \
+#define define_integer(value)                                  \
+                                                               \
+void *                                                         \
+integer_ ## value ## _rack[] =                                 \
+  {                                                            \
+    (void *) &symbol_nil_header,                               \
+    (void *) &integer_1_header,                                \
+    (void *) value,                                            \
+  };                                                           \
+                                                               \
+struct header_struct integer_ ## value ## _header =            \
+  {                                                            \
+    &class_integer_header,                                     \
+    integer_ ## value ## _rack,		                       \
   }
 
-#define reference_integer(value)                     \
+#define reference_integer(value)                               \
   (void *) &integer_ ## value ## _header
+
+#define declare_integer(value)                                 \
+  extern struct header_struct integer_ ## value ## _header;
