98928492d95ee740cf94e3ae1a10acef35f6f498
Robert Strandh <robert.strandh@gmail.com>
2016-05-30 14:03:26 +0200

    Define macros for definining, declaring, and referring to symbols.

--- class_symbol.h
+++ class_symbol.h
@@ -1,9 +1,30 @@
 #ifndef CLASS_SYMBOL_H
 #define CLASS_SYMBOL_H
 
+#include "class_string.h"
+
 extern struct header_struct class_symbol_header;
 
-#define reference_symbol(name)                       \
+#define define_symbol(name, package)			    \
+                                                            \
+void *                                                      \
+symbol_ ## name ## _rack[] =                                \
+  {                                                         \
+    reference_symbol(NIL),                                  \
+    reference_string(name),			            \
+    reference_symbol(NIL),                                  \
+  };                                                        \
+                                                            \
+struct header_struct symbol_ ## name ## _header =           \
+  {                                                         \
+    reference_symbol(NIL),	 		            \
+    symbol_ ## name ## _rack,		                    \
+  };
+
+#define declare_symbol(name)                                \
+  extern struct header_struct symbol_ ## name ## _header;
+
+#define reference_symbol(name)                              \
   (void *) &symbol_ ## name ## _header
 
 #endif
