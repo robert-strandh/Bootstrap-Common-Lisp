6897605518579b82f4196c4686fe562f48832878
Robert Strandh <robert.strandh@gmail.com>
2016-05-30 11:00:51 +0200

    Add macro for declaring a string.

--- class_character.h
+++ class_character.h
@@ -3,21 +3,24 @@
 
 extern struct header_struct class_character_header;
 
-#define define_character(name, value)		       \
-                                                       \
-void *                                                 \
-character_ ## name ## _rack[] =                        \
-  {                                                    \
-    (void *) &symbol_nil_header,                       \
-    (void *) &integer_1_header,                        \
-    (void *) value,                                    \
-  };                                                   \
-                                                       \
-struct header_struct character_ ## name ## _header =   \
-  {                                                    \
-    &class_character_header,                           \
-    character_ ## name ## _rack,		       \
+#define define_character(name, value)		                 \
+                                                                 \
+void *                                                           \
+character_ ## name ## _rack[] =                                  \
+  {                                                              \
+    (void *) &symbol_nil_header,                                 \
+    (void *) &integer_1_header,                                  \
+    (void *) value,                                              \
+  };                                                             \
+                                                                 \
+struct header_struct character_ ## name ## _header =             \
+  {                                                              \
+    &class_character_header,                                     \
+    character_ ## name ## _rack,		                 \
   }
 
-#define reference_character(name)                      \
+#define reference_character(name)                                \
   (void *) &character_ ## name ## _header
+
+#define declare_character(name)                                  \
+  extern struct header_struct character_ ## name ## _header;
