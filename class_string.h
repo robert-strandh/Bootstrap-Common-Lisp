50eb8fed31a4cfe22f2713d8a6a363c75b6f3dbf
Robert Strandh <robert.strandh@gmail.com>
2016-05-30 11:13:10 +0200

    Add macro for declaring a string.

--- class_string.h
+++ class_string.h
@@ -6,4 +6,7 @@ extern struct header_struct class_string_header;
 #define reference_string(name)                       \
   (void *) &string_ ## name ## _header
 
+#define declare_string(name)                                  \
+  extern struct header_struct string_ ## name ## _header;
+
 #endif
