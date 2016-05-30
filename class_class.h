8f1eb672324b3ae12732aeafadb3fc1f82ef2427
Robert Strandh <robert.strandh@gmail.com>
2016-05-30 14:02:22 +0200

    Define macros for defining, declaring, and referring to classes.

--- class_class.h
+++ class_class.h
@@ -1,4 +1,24 @@
 #include "header.h"
+#include "symbols.h"
 
-#define reference_class(name)                        \
+extern struct header_struct class_class_header;
+
+#define define_class(name)                                  \
+                                                            \
+void *                                                      \
+class_ ## name ## _rack[] =                                 \
+  {                                                         \
+    reference_symbol(NIL),                                  \
+  };                                                        \
+                                                            \
+struct header_struct class_ ## name ## _header =            \
+  {                                                         \
+    &class_class_header,                                    \
+    class_ ## name ## _rack,		                    \
+  }
+
+#define declare_class(name)                                 \
+  extern struct header_struct class_ ## name ## _header;
+
+#define reference_class(name)                               \
   (void *) &class_ ## name ## _header
