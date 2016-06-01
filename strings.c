ac4a2a990bfa34f3363e261f006efeeee9a96e3f
Robert Strandh <robert.strandh@gmail.com>
2016-06-01 19:16:31 +0200

    Define header and rack for string T.

--- strings.c
+++ strings.c
@@ -20,3 +20,17 @@ struct header_struct string_NIL_header =
     reference_symbol(NIL),
     string_NIL_rack,
   };
+
+void *
+string_T_rack[] =
+  {
+    reference_symbol(NIL),
+    &integer_1_header,
+    &character_T_header,
+  };
+
+struct header_struct string_T_header =
+  {
+    reference_symbol(NIL),
+    string_T_rack,
+  };
