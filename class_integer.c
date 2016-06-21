afca355ff19dc58f021e6bd5210a03be5ff6119e
Robert Strandh <robert.strandh@gmail.com>
2016-06-21 15:12:33 +0200

    Remove use of macro define_class for the definition of the class INTEGER.

--- class_integer.c
+++ class_integer.c
@@ -1,3 +1,13 @@
 #include "class_class.h"
 
-define_class(integer, built_in_class);
+void *
+class_integer_rack[] =
+  {
+    reference_symbol(NIL),
+  };
+
+struct header_struct class_integer_header =
+  {
+    &class_built_in_class_header,
+    class_integer_rack,
+  };
