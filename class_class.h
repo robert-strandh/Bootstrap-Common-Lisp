b9f269b2decdc7cba9ede9051940236145dd13c5
Robert Strandh <robert.strandh@gmail.com>
2016-06-21 15:15:08 +0200

    Remove macro define_class.  It can't be used effectively anyway.

--- class_class.h
+++ class_class.h
@@ -3,20 +3,6 @@
 
 extern struct header_struct class_built_in_class_header;
 
-#define define_class(name, metaclass_name)	            \
-                                                            \
-void *                                                      \
-class_ ## name ## _rack[] =                                 \
-  {                                                         \
-    reference_symbol(NIL),                                  \
-  };                                                        \
-                                                            \
-struct header_struct class_ ## name ## _header =            \
-  {                                                         \
-    &class_ ## metaclass_name ## _header,                   \
-    class_ ## name ## _rack,		                    \
-  }
-
 #define declare_class(name)                                 \
   extern struct header_struct class_ ## name ## _header;
 
